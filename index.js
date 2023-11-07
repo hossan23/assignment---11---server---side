376fa0892d1b7ba3d7aa3b81d69aa4cf206489f4
hossan23 <imamhossan101@gmail.com>
2023-11-08 00:42:55 +0600

    successfully added jwt for securing api

--- index.js
+++ index.js
@@ -34,7 +34,18 @@ app.post('/jwt', async (req, res) => {
  const user = req.body;
  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
 
- res.send({ token });
+ res
+  .cookie('token', token, {
+   httpOnly: true,
+   secure: true,
+   sameSite: 'none',
+  })
+  .send({ success: true });
+});
+
+app.post('/logout', async (req, res) => {
+ const user = req.body;
+ res.clearCookie('token', { maxAge: 0 }).send({ success: true });
 });
 
 // database collections
